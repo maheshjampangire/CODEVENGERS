6e53eba0c575e9fdc219178da602e4a5f8840e1c
maheshjampangire <157024613+maheshjampangire@users.noreply.github.com>
2024-07-18 16:11:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.codevengers;
+
+import com.codevengers.initialpage.InitialApp;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(InitialApp.class, args);
+    }
+}
